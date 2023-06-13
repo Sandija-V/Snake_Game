e130d8181a93f37a53187dcd9c0e9d48b41f0725
Sandija-V <128511246+Sandija-V@users.noreply.github.com>
2023-06-13 16:20:35 +0300

    Trying to implement registration window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
 #include <cstdlib> // For rand()
 #include <sstream> // For std::stringstream
 
@@ -274,80 +277,430 @@ private:
 	bool paused = false;
 };
 
+    class LoginScreen {
+private:
+        sf::RenderWindow window;
+    sf::Font font;
+    sf::Text title;
+    sf::Text usernameLabel;
+    sf::Text passwordLabel;
+    sf::Text usernameInput;
+    sf::Text passwordInput;
+    sf::Text loginButton;
+	sf::Text registerButton;
+    std::string username;
+    std::string password;
+    bool loginSuccess;
+
+public:
+    LoginScreen() : window(sf::VideoMode(800, 600), "Snake Game"), loginSuccess(false) {
+        // Load the font
+        if (!font.loadFromFile("D:/sfml/Snake_Game/src/Poppins-Medium.ttf")) {
+            std::cout << "Error loading font\n";
+        }
+
+        title.setFont(font);
+        title.setString("Register for the game");
+        title.setCharacterSize(34);
+        title.setFillColor(sf::Color::White);
+        title.setPosition(100, 100);
+
+        usernameLabel.setFont(font);
+        usernameLabel.setString("Username");
+        usernameLabel.setCharacterSize(34);
+        usernameLabel.setFillColor(sf::Color::White);
+        usernameLabel.setPosition(100, 150);
+
+        passwordLabel.setFont(font);
+        passwordLabel.setString("Password");
+        passwordLabel.setCharacterSize(34);
+        passwordLabel.setFillColor(sf::Color::White);
+        passwordLabel.setPosition(100, 200);
+
+        usernameInput.setFont(font);
+        usernameInput.setCharacterSize(34);
+        usernameInput.setFillColor(sf::Color::White);
+        usernameInput.setPosition(300, 150);
+
+        passwordInput.setFont(font);
+        passwordInput.setCharacterSize(34);
+        passwordInput.setFillColor(sf::Color::White);
+        passwordInput.setPosition(300, 200);
+
+        loginButton.setFont(font);
+        loginButton.setString("Login");
+        loginButton.setCharacterSize(34);
+        loginButton.setFillColor(sf::Color::White);
+        loginButton.setPosition(100, 250);
+
+		registerButton.setFont(font);
+        registerButton.setString("Register");
+        registerButton.setCharacterSize(34);
+        registerButton.setFillColor(sf::Color::White);
+        registerButton.setPosition(350, 250);
+
+		// Make the username input field active so that user could actually type in username
+		usernameInput.setString("");
+		usernameInput.setFillColor(sf::Color::Black);
+
+		// Make the password input field active so that user could actually type in password
+		passwordInput.setString("");
+		passwordInput.setFillColor(sf::Color::Black);
+
+    }
+
+    void run() {
+        while (window.isOpen()) {
+            handleEvents();
+
+            window.clear();
+            window.draw(title);
+            window.draw(usernameLabel);
+            window.draw(passwordLabel);
+
+            // Draw the username input field
+            sf::RectangleShape usernameField(sf::Vector2f(200, 34));
+            usernameField.setPosition(300, 150);
+            usernameField.setFillColor(sf::Color::White);
+            window.draw(usernameField);
+            window.draw(usernameInput);
+
+            // Draw the password input field
+            sf::RectangleShape passwordField(sf::Vector2f(200, 34));
+            passwordField.setPosition(300, 200);
+            passwordField.setFillColor(sf::Color::White);
+            window.draw(passwordField);
+            window.draw(passwordInput);
+
+            window.draw(loginButton);
+			window.draw(registerButton);
+            window.display();
+
+            if (loginSuccess) {
+                // Start the game
+                startGame();
+                break;
+            }
+        }
+    }
+
+private:
+    void handleEvents() {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode < 128) {
+                    handleTextInput(static_cast<char>(event.text.unicode));
+                }
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Enter) {
+                    validateCredentials();
+                }
+            }
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    if (loginButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                        validateCredentials();
+                    }
+                }
+            }
+        }
+    }
+
+    void handleTextInput(char inputChar) {
+        if (usernameInput.getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
+            if (inputChar == '\b') {
+                // Remove the last character from the input string
+                if (!username.empty()) {
+                    username.pop_back();
+                }
+            }
+            else if (inputChar == '\r') {
+                validateCredentials();
+            }
+            else {
+                // Append the input character to the username string
+                username += inputChar;
+            }
+            usernameInput.setString(username);
+        }
+        else if (passwordInput.getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
+            if (inputChar == '\b') {
+                // Remove the last character from the input string
+                if (!password.empty()) {
+                    password.pop_back();
+                }
+            }
+            else if (inputChar == '\r') {
+                validateCredentials();
+            }
+            else {
+                // Append the input character to the password string
+                password += inputChar;
+            }
+            // Replace the characters with asterisks for password masking
+            passwordInput.setString(std::string(password.length(), '*'));
+        }
+    }
+
+    void validateCredentials() {
+        // Implement your own validation logic here
+        // For simplicity, let's consider the credentials valid if both fields are non-empty
+        if (!username.empty() && !password.empty()) {
+            loginSuccess = true;
+        }
+    }
+
+    void startGame() {
+        // Create the game window
+        sf::RenderWindow gameWindow(sf::VideoMode(800, 600), "Snake Game");
+
+        // Create the snake object
+        Snake snake(10, 10, 8, 5, 1);
+        Food food(5, 5);
+
+        // Initialize clock
+        sf::Clock clock;
+        sf::Time lt = clock.getElapsedTime();
+
+        // Game loop
+        while (gameWindow.isOpen()) {
+            // Handle events
+            sf::Event event;
+            while (gameWindow.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    gameWindow.close();
+                }
+                else if (event.type == sf::Event::KeyPressed) {
+                    if (event.key.code == sf::Keyboard::Up && snake.getDirection() != 2) {
+                        snake.setDirection(0);
+                    }
+                    else if (event.key.code == sf::Keyboard::Right && snake.getDirection() != 3) {
+                        snake.setDirection(1);
+                    }
+                    else if (event.key.code == sf::Keyboard::Down && snake.getDirection() != 0) {
+                        snake.setDirection(2);
+                    }
+                    else if (event.key.code == sf::Keyboard::Left && snake.getDirection() != 1) {
+                        snake.setDirection(3);
+                    }
+                    else if (event.key.code == sf::Keyboard::Space) {
+                        if (snake.isPaused()) {
+                            snake.start();
+                        }
+                        else {
+                            snake.pause();
+                        }
+                    }
+                }
+            }
+
+            // Clear the window
+            gameWindow.clear();
+
+            if (!snake.isPaused()) {
+                // Get difference from lt to now
+                sf::Time now = clock.getElapsedTime();
+                sf::Time diff = now - lt;
+                if (diff.asMilliseconds() > 1000 / snake.getSpeed()) {
+                    lt = now;
+                    snake.move();
+                }
+            }
+
+            // Draw the snake
+            snake.draw(gameWindow);
+            // Draw the food
+            food.draw(gameWindow);
+
+            (snake.checkCollision(food));
+
+            if (snake.checkCollisionWithItself()) {
+                snake = Snake(10, 10, 8, 5, 1);
+            }
+
+            // Update the window
+            gameWindow.display();
+        }
+    }
+};
 
 int main() {
 
-	// Create the game window
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Snake Game");
+	// //Create a login window.
+	// sf::RenderWindow loginWindow(sf::VideoMode(800, 600), "Snake Game");
+	// sf::Font font;
+	// if (!font.loadFromFile("D:/sfml/Snake_Game/src/Poppins-Medium.ttf")) {
+	// 	std::cout << "Error loading font\n";
+	// }
 
-	// Create the snake object
-	Snake snake(10, 10, 8, 5, 1);
-	Food food(5, 5);
+	// sf::Text text;
+	// text.setFont(font);
+	// text.setString("Press Enter to start the game");
+	// text.setCharacterSize(34);
+	// text.setFillColor(sf::Color::White);
+	// text.setPosition(150, 200);
 
-    // init clock
-    sf::Clock clock;
-    sf::Time lt = clock.getElapsedTime();
+	// // Game loop
+	// while (loginWindow.isOpen()) {
+	// 	// Handle events
+	// 	sf::Event event;
+	// 	while (loginWindow.pollEvent(event)) {
+	// 		if (event.type == sf::Event::Closed) {
+	// 			loginWindow.close();
+	// 		}
+	// 		else if (event.type == sf::Event::KeyPressed) {
+	// 			if (event.key.code == sf::Keyboard::Enter) {
+	// 				loginWindow.close();
+	// 				// break;
+	// 			}
+	// 		}
+	// 	}
 
-	// Game loop
-	while (window.isOpen()) {
-		// Handle events
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-			else if (event.type == sf::Event::KeyPressed) {
-				if (event.key.code == sf::Keyboard::Up && snake.getDirection() != 2) {
-					snake.setDirection(0);
-				}
-				else if (event.key.code == sf::Keyboard::Right && snake.getDirection() != 3) {
-					snake.setDirection(1);
-				}
-				else if (event.key.code == sf::Keyboard::Down && snake.getDirection() != 0) {
-					snake.setDirection(2);
-				}
-				else if (event.key.code == sf::Keyboard::Left && snake.getDirection() != 1) {
-					snake.setDirection(3);
-				}
-				else if (event.key.code == sf::Keyboard::Space) {
-                    if (snake.isPaused()) {
-                        snake.start();
-                    }
-                    else {
-                        snake.pause();
-					}
-				}
-			}
-		}
+	// 	// Clear the window
+	// 	loginWindow.clear();
 
-		// Clear the window
-		window.clear();
+	// 	// Draw the text
+	// 	loginWindow.draw(text);
 
-		if (!snake.isPaused()) {
-			//ged diference from lt to now
-            sf::Time now = clock.getElapsedTime();
-			sf::Time diff = now - lt;
-			if (diff.asMilliseconds() > 1000 / snake.getSpeed()) {
-				lt = now;
-				snake.move();
-			}
-        }
+	// 	// Display the window
+	// 	loginWindow.display();
+	// }
 
-		// Draw the snake
-		snake.draw(window);
-		// Draw the food
-		food.draw(window);
+	// // Create a window where the player has to register for the game. A username and password has to be entered and saved in the database.
+	// sf::RenderWindow registerWindow(sf::VideoMode(800, 600), "Snake Game");
+	// sf::Font font;
+	// if (!font.loadFromFile("D:/sfml/Snake_Game/src/Poppins-Medium.ttf")) {
+	// 	std::cout << "Error loading font\n";
+	// }
 
-		(snake.checkCollision(food));
+	// sf::Text text;
+	// text.setFont(font);
+	// text.setString("Register for the game");
+	// text.setCharacterSize(34);
+	// text.setFillColor(sf::Color::White);
+	// text.setPosition(100, 100);
 
-		if(snake.checkCollisionWithItself()) {
-			snake = Snake(10, 10, 8, 5, 1);
-		}
+	// sf::Text username;
+	// text.setFont(font);
+	// text.setString("Username");
+	// text.setCharacterSize(34);
+	// text.setFillColor(sf::Color::White);
+	// text.setPosition(100, 150);
 
-		// Update the window
-		window.display();
-		//sf::sleep(sf::milliseconds(1000 / (snake.getSpeed())));
-	}
+	// sf::Text password;
+	// text.setFont(font);
+	// text.setString("Password");
+	// text.setCharacterSize(34);
+	// text.setFillColor(sf::Color::White);
+	// text.setPosition(100, 200);
+
+	// // Game loop
+	// 	while (registerWindow.isOpen()) {
+	// 	// Handle events
+	// 	sf::Event event;
+	// 	while (registerWindow.pollEvent(event)) {
+	// 		if (event.type == sf::Event::Closed) {
+	// 			registerWindow.close();
+	// 		}
+	// 		else if (event.type == sf::Event::KeyPressed) {
+	// 			if (event.key.code == sf::Keyboard::Enter) {
+	// 				registerWindow.close();
+	// 				// break;
+	// 			}
+	// 		}
+	// 	}
+
+	// 	// Clear the window
+	// 	registerWindow.clear();
+
+	// 	// Draw the text
+	// 	registerWindow.draw(text);
+
+	// 	// Display the window
+	// 	registerWindow.display();
+	// }
+
+
+	// // Create the game window
+	// sf::RenderWindow window(sf::VideoMode(800, 600), "Snake Game");
+
+	// // Create the snake object
+	// Snake snake(10, 10, 8, 5, 1);
+	// Food food(5, 5);
+
+    // // init clock
+    // sf::Clock clock;
+    // sf::Time lt = clock.getElapsedTime();
+
+	// // Game loop
+	// while (window.isOpen()) {
+	// 	// Handle events
+	// 	sf::Event event;
+	// 	while (window.pollEvent(event)) {
+	// 		if (event.type == sf::Event::Closed) {
+	// 			window.close();
+	// 		}
+	// 		else if (event.type == sf::Event::KeyPressed) {
+	// 			if (event.key.code == sf::Keyboard::Up && snake.getDirection() != 2) {
+	// 				snake.setDirection(0);
+	// 			}
+	// 			else if (event.key.code == sf::Keyboard::Right && snake.getDirection() != 3) {
+	// 				snake.setDirection(1);
+	// 			}
+	// 			else if (event.key.code == sf::Keyboard::Down && snake.getDirection() != 0) {
+	// 				snake.setDirection(2);
+	// 			}
+	// 			else if (event.key.code == sf::Keyboard::Left && snake.getDirection() != 1) {
+	// 				snake.setDirection(3);
+	// 			}
+	// 			else if (event.key.code == sf::Keyboard::Space) {
+    //                 if (snake.isPaused()) {
+    //                     snake.start();
+    //                 }
+    //                 else {
+    //                     snake.pause();
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+
+	// 	// Clear the window
+	// 	window.clear();
+
+	// 	if (!snake.isPaused()) {
+	// 		//ged diference from lt to now
+    //         sf::Time now = clock.getElapsedTime();
+	// 		sf::Time diff = now - lt;
+	// 		if (diff.asMilliseconds() > 1000 / snake.getSpeed()) {
+	// 			lt = now;
+	// 			snake.move();
+	// 		}
+    //     }
+
+	// 	// Draw the snake
+	// 	snake.draw(window);
+	// 	// Draw the food
+	// 	food.draw(window);
+
+	// 	(snake.checkCollision(food));
+
+	// 	if(snake.checkCollisionWithItself()) {
+	// 		snake = Snake(10, 10, 8, 5, 1);
+	// 	}
+
+	// 	// Update the window
+	// 	window.display();
+	// 	//sf::sleep(sf::milliseconds(1000 / (snake.getSpeed())));
+	// }
+
+	LoginScreen loginScreen;
+    loginScreen.run();
 
 	return 0;
 }
